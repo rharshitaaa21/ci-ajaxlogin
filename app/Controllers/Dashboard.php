140aa153c1308bc35078939aeaae4a6cae779b53
Harshita Raghav <raghav.harshita21@gmail.com>
2023-04-16 02:17:14 +0530

    Updated changes

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,24 +1,21 @@
 <?php
-
 namespace App\Controllers;
-
+// use App\Controllers\Login;
 class Dashboard extends BaseController
 {
     public function __construct()
     {
         $this->session = \Config\Services::session();
     }
-
     public function index()
     {
-        // Check if user is logged in
-        if ( !$this->session->get('isLoggedIn'))
-        {
-            return redirect()->to('/login');
-        }
-        else{echo view('dashboard');}
 
-        // Display dashboard page
-        
+        // print_r($session);
+        if (!$this->session->has('isLoggedIn') || !$this->session->get('isLoggedIn')) {
+            return redirect()->to('/login');
+        } else {
+            echo view('dashboard');
+        }
     }
+    
 }
--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -4,40 +4,52 @@ namespace App\Controllers;
 
 use App\Models\UserModel;
 
+
 class Login extends BaseController
 {
     public function __construct()
     {
-        $this->session = \Config\Services::session();
+         
+        $this->session 	= \Config\Services::session();
+        //  print_r($this->session);
     }
 
     public function index()
     {
         // Display login page
         echo view('login');
+       
     }
 
     public function do_login()
     {
+      
         $userModel = new UserModel();
-
         $email = $this->request->getPost('email');
         $password = $this->request->getPost('password');
-
         $result = $userModel->where('email', $email)->first();
+
+        // print_r($userModel);
 
         if ($result)
         {
             if (password_verify($password, $result->password))
             {
-                // Set user data in session
+                // var_dump($password);
+                // var_dump($result);
                 $userData = [
                     'id' => $result->id,
-                    'name' => $result->name,
-                    'email' => $result->email,
-                    'isLoggedIn' => true
+                     'isLoggedIn' => true
                 ];
+                // print_r($result->id);
+                // print_r($userData);
+                $this->session->setFlashdata('name',$result->name);
+
+                // var_dump($result);
+                // var_dump($result->name);
+
                 $this->session->set($userData);
+                
                 return redirect()->to('/dashboard');
             }
             else
@@ -56,10 +68,13 @@ class Login extends BaseController
 
     public function logout()
     {
-        // Destroy session
-        $this->session->destroy();
+      
+    $this->session->remove('isLoggedIn');
+    $this->session->destroy();
 
-        // Redirect to login page
-        return redirect()->to('/login');
-    }
+    // Redirect to login page
+    return redirect()->to('/login');
+}
+
+    
 }
